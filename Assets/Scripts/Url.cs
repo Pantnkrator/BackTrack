3d7cf12a48d4ae9c43f0ae14c33431c562f8ab1f
oarr <oscar.rondo12@gmail.com>
2017-11-06 03:01:39 -0400

    url button, urlObject, url script

---
+++ Assets/Scripts/Url.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Url : MonoBehaviour {
+
+	public void UrlLinkOnWeb(){
+		Application.OpenURL ("https://www.google.com/");  
+	}
+}
